74f110f331db3a1c96f19fa52033bc66503040cd
santiago <agsantiago030102@outlook.com>
2025-10-09 18:45:47 -0500

    Actualización de configuración y dependencias

--- src/main/java/com/fly/company/f4u_backend/config/WebConfig.java
+++ src/main/java/com/fly/company/f4u_backend/config/WebConfig.java
@@ -1,14 +1,15 @@
 package com.fly.company.f4u_backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://<TU_FRONTEND>.azurestaticapps.net")
+                .allowedOrigins("https://delightful-tree-0a83b250f.1.azurestaticapps.net")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowCredentials(true);
     }
